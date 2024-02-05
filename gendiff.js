d618f4be417de7587596a90652df22ff1d522427
Circe Choi (foxyfable) <swinefoxy@gmail.com>
2024-02-05 11:33:47 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -5,5 +5,8 @@ program
   .name('gendiff')
   .version('1.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse();
