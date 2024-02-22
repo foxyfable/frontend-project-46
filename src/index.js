93538f89b0c000f86ae2b65a54eec30858adc6c5
Circe Choi (foxyfable) <swinefoxy@gmail.com>
2024-02-22 21:49:26 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import parse from './parse.js';
+import _ from 'lodash';
 
 const getFixturePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
@@ -14,7 +15,24 @@ const gendiff = (path1, path2, type) => {
   const file1 = parse(data1, getFiletype(path1));
   const file2 = parse(data2, getFiletype(path2));
 
-  return `${JSON.stringify(file1)} ${JSON.stringify(file2)}`;
+  const sortedKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+  const result = sortedKeys.map((key) => {
+    if (!Object.hasOwn(file1, key)) {
+      return `+ ${key}: ${file2[key]}`;
+    }
+    if (!Object.hasOwn(file2, key)) {
+      return `- ${key}: ${file1[key]}`;
+    }
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      if (_.isEqual(file1[key], file2[key])) {
+        return `  ${key}: ${file1[key]}`;
+      }
+      return `- ${key}: ${file1[key]}
++ ${key}: ${file2[key]}`;
+    }
+  });
+
+  return `${JSON.stringify(result)}`;
 };
 
 export default gendiff;
