5c007c25e7064494b0793339f11a51393f8ca386
Circe Choi (foxyfable) <swinefoxy@gmail.com>
2024-02-23 16:22:02 +0300

    add tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import parse from './parse.js';
 import _ from 'lodash';
+import parse from './parse.js';
 
 const getFixturePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
@@ -18,22 +18,21 @@ const gendiff = (path1, path2, type) => {
   const sortedKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
   const resultArr = sortedKeys.map((key) => {
     if (!Object.hasOwn(file1, key)) {
-      return {[`+ ${key}`]: file2[key]};
+      return { [`+ ${key}`]: file2[key] };
     }
     if (!Object.hasOwn(file2, key)) {
-      return {[`- ${key}`]: file1[key]};
+      return { [`- ${key}`]: file1[key] };
     }
     if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
       if (_.isEqual(file1[key], file2[key])) {
-        return {[`  ${key}`]: file2[key]};
+        return { [`  ${key}`]: file2[key] };
       }
-      return {[`- ${key}`]: file1[key], [`+ ${key}`]: file2[key]};
+      return { [`- ${key}`]: file1[key], [`+ ${key}`]: file2[key] };
     }
+    return null;
   });
 
-  const resultObj = resultArr.reduce(
-    (obj, item) => Object.assign(obj, {...item}), {});
-
+  const resultObj = resultArr.reduce((obj, item) => Object.assign(obj, { ...item }), {});
   const formattedResult = (JSON.stringify(resultObj, null, ' '.repeat(2)).replace(/[",]/g, ''));
 
   return formattedResult;
